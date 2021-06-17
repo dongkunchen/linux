32650e4101cee3236fb08d314f58a12a2612f0c1
dongkunchen <85998522+dongkunchen@users.noreply.github.com>
2021-06-17 12:10:11 +0800

    Update signal.c

--- signal.c
+++ signal.c
@@ -1,4 +1,3 @@
-//signal函數測試給沒有讀端的信號產生SIGPIPE信號
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,7 +6,6 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 
-//信號處理函數
 void sighandler(int signo)
 {
     printf("signo==[%d]\n", signo);
@@ -15,7 +13,6 @@ void sighandler(int signo)
 
 int main()
 {
-    //創建管道int pipe(int pipefd[2]);
     int fd[2];
     int ret = pipe(fd);
     if (ret < 0)
@@ -24,10 +21,8 @@ int main()
         return -1;
     }
     
-    //註冊SIGPIPE信號處理函數
     signal(SIGPIPE, sighandler);
 
-    //關閉讀端
     close(fd[0]);
 
     write(fd[1], "hello world", strlen("hello world"));
