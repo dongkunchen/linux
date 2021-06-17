00909f251e2fa16c44c497df49280984a23d9d06
dongkunchen <85998522+dongkunchen@users.noreply.github.com>
2021-06-17 11:57:45 +0800

    Update pub.c

--- pub.c
+++ pub.c
@@ -1,21 +1,10 @@
 #include "pub.h"
-//通过文件名字获得文件类型
 char *get_mime_type(char *name)
 {
     char* dot;
 
-    dot = strrchr(name, '.');	//自右向左查找‘.’字符;如不存在返回NULL
-    /*
-     *charset=iso-8859-1	西欧的编码，说明网站采用的编码是英文；
-     *charset=gb2312		说明网站采用的编码是简体中文；
-     *charset=utf-8			代表世界通用的语言编码；
-     *						可以用到中文、韩文、日文等世界上所有语言编码上
-     *charset=euc-kr		说明网站采用的编码是韩文；
-     *charset=big5			说明网站采用的编码是繁体中文；
-     *
-     *以下是依据传递进来的文件名，使用后缀判断是何种文件类型
-     *将对应的文件类型按照http定义的关键字发送回去
-     */
+    dot = strrchr(name, '.');	
+ 
     if (dot == (char*)0)
         return "text/plain; charset=utf-8";
     if (strcmp(dot, ".html") == 0 || strcmp(dot, ".htm") == 0)
@@ -51,20 +40,8 @@ char *get_mime_type(char *name)
 
     return "text/plain; charset=utf-8";
 }
-/**********************************************************************/
-/* Get a line from a socket, whether the line ends in a newline,
- * carriage return, or a CRLF combination.  Terminates the string read
- * with a null character.  If no newline indicator is found before the
- * end of the buffer, the string is terminated with a null.  If any of
- * the above three line terminators is read, the last character of the
- * string will be a linefeed and the string will be terminated with a
- * null character.
- * Parameters: the socket descriptor
- *             the buffer to save the data in
- *             the size of the buffer
- * Returns: the number of bytes stored (excluding null) */
-/**********************************************************************/
-//获得一行数据，每行以\r\n作为结束标记
+
+
 int get_line(int sock, char *buf, int size)
 {
     int i = 0;
@@ -74,13 +51,12 @@ int get_line(int sock, char *buf, int size)
     while ((i < size - 1) && (c != '\n'))
     {
         n = recv(sock, &c, 1, 0);
-        /* DEBUG printf("%02X\n", c); */
+       
         if (n > 0)
         {
             if (c == '\r')
             {
-                n = recv(sock, &c, 1, MSG_PEEK);//MSG_PEEK 从缓冲区读数据，但是数据不从缓冲区清除
-                /* DEBUG printf("%02X\n", c); */
+                n = recv(sock, &c, 1, MSG_PEEK);
                 if ((n > 0) && (c == '\n'))
                     recv(sock, &c, 1, 0);
                 else
@@ -97,29 +73,22 @@ int get_line(int sock, char *buf, int size)
     return(i);
 }
 
-//下面的函数第二天使用
-/*
- * 这里的内容是处理%20之类的东西！是"解码"过程。
- * %20 URL编码中的‘ ’(space)
- * %21 '!' %22 '"' %23 '#' %24 '$'
- * %25 '%' %26 '&' %27 ''' %28 '('......
- * 相关知识html中的‘ ’(space)是&nbsp
- */
+
 void strdecode(char *to, char *from)
 {
     for ( ; *from != '\0'; ++to, ++from) {
 
-        if (from[0] == '%' && isxdigit(from[1]) && isxdigit(from[2])) { //依次判断from中 %20 三个字符
+        if (from[0] == '%' && isxdigit(from[1]) && isxdigit(from[2])) { 
 
-            *to = hexit(from[1])*16 + hexit(from[2]);//字符串E8变成了真正的16进制的E8
-            from += 2;                      //移过已经处理的两个字符(%21指针指向1),表达式3的++from还会再向后移一个字符
+            *to = hexit(from[1])*16 + hexit(from[2]);
+            from += 2;                      
         } else
             *to = *from;
     }
     *to = '\0';
 }
 
-//16进制数转化为10进制, return 0不会出现
+
 int hexit(char c)
 {
     if (c >= '0' && c <= '9')
@@ -132,8 +101,7 @@ int hexit(char c)
     return 0;
 }
 
-//"编码"，用作回写浏览器的时候，将除字母数字及/_.-~以外的字符转义后回写。
-//strencode(encoded_name, sizeof(encoded_name), name);
+
 void strencode(char* to, size_t tosize, const char* from)
 {
     int tolen;
