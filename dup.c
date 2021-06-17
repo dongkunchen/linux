8b51712aefdc7c3ac6cee183dab57f4a27d5396a
dongkunchen <85998522+dongkunchen@users.noreply.github.com>
2021-06-17 11:00:26 +0800

    Update dup.c

--- dup.c
+++ dup.c
@@ -14,23 +14,23 @@ int main(int argc, char *argv[])
         perror("open error");
         return -1;
     }
-    //調用dup函數複製fd
+    
     int newfd = dup(fd);
     printf("newfd:[%d], fd:[%d]\n", newfd, fd);
 
-    //使用fd對文件進行寫的操作
+    
     write(fd, "hello world", strlen("hello world"));
 
-    //調用lseek函數移動文件指針到開始處
+    
     lseek(fd, 0, SEEK_SET);
 
-    //使用newfd讀文件
+    
     char buf[64];
     memset(buf, 0x00, sizeof(buf));
     int n = read(newfd, buf, sizeof(buf));
     printf("read over: n==[%d], buf==[%s]\n", n, buf);
 
-    //關閉文件
+   
     close(fd);
     close(newfd);
 
