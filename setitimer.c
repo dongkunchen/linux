82e099886e525aeee8d5a51a1df3c02fe38ffae3
dongkunchen <85998522+dongkunchen@users.noreply.github.com>
2021-06-17 12:08:15 +0800

    Update setitimer.c

--- setitimer.c
+++ setitimer.c
@@ -1,4 +1,3 @@
-//raise函數測試
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,7 +6,6 @@
 #include <signal.h>
 #include <sys/time.h>
 
-//SIGALRM信號處理函數
 void sighandler(int signo)
 {
     printf("signo==[%d]\n", signo);
@@ -15,16 +13,11 @@ void sighandler(int signo)
 
 int main()
 {
-    //註冊信號SIGALRM信號處理函數
     signal(SIGALRM, sighandler);
     
-    // int setitimer(int which, const struct itimerval *new_value,
-    //                  struct itimerval *old_value);
     struct itimerval tm;
-    //週期性時間賦值
     tm.it_interval.tv_sec = 1;
     tm.it_interval.tv_usec = 0;
-    //第一次觸發的時間
     tm.it_value.tv_sec = 3;
     tm.it_value.tv_usec = 0;
 
