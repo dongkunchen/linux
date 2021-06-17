7bf8ecf0306b72a888afb8f83b8d47b34934c324
dongkunchen <85998522+dongkunchen@users.noreply.github.com>
2021-06-17 11:34:04 +0800

    Update pthread_exit.c

--- pthread_exit.c
+++ pthread_exit.c
@@ -1,4 +1,3 @@
-//線程退出
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,7 +11,7 @@ void *mythread(void *arg)
 {
     printf("child thread, pid==[%d], id==[%ld]\n", getpid(), pthread_self());
     printf("[%p]\n", &g_var);
-    pthread_exit(&g_var);//不影響主線程
+    pthread_exit(&g_var);
 }
 
 int main()
@@ -26,14 +25,11 @@ int main()
     }
     printf("main thread, pid==[%d], id==[%ld]\n", getpid(), pthread_self());
 
-    //回收子線程
     void *p = NULL;
     pthread_join(thread, &p);
     int n = *(int *)p;
     printf("child exit status:[%d], [%p]\n", n, p);
-    //pthread_exit(NULL);//回收主線程會產生殭屍進程
 
-    //目的是為了回收前讓子線程執行起來
    
     return 0;
 }
