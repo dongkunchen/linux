395cca59170cf8d087e4d888a6d6ac421f46530b
dongkunchen <85998522+dongkunchen@users.noreply.github.com>
2021-06-17 11:15:43 +0800

    Update kill1.c

--- kill1.c
+++ kill1.c
@@ -1,4 +1,3 @@
-//kill函數測試:父進程殺死子進程 或 子進程殺死父進程
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -20,11 +19,7 @@ int main()
 		else if (pid > 0)
 		{
 			printf("father: pid==[%d], fpid==[%d]\n", getpid(), getppid());
-			//父進程殺死第一個子進程
-			// if(i==0)
-			// {
-			// 	kill(pid,SIGKILL);
-			// }
+			
 		}
 		else if (pid == 0)
 		{
@@ -33,30 +28,25 @@ int main()
 		}
 	}
 
-	if (i == 0) //第一個子進程
+	if (i == 0)
 	{
 		printf("the first child, pid==[%d]\n", getpid());
-		//測試子進程殺死父進程
-		//kill(getppid(),SIGKILL);
-		// sleep(10);
 		sleep(1);
-		//0殺死同一組
-		//kill(0,SIGKILL); //-1很危險連窗口都殺了
 	}
 
-	if (i == 1) //第二個子進程
+	if (i == 1) 
 	{
 		printf("the second child, pid==[%d]\n", getpid());
 		sleep(10);
 	}
 
-	if (i == 2) //第三個子進程
+	if (i == 2)
 	{
 		printf("the third child, pid==[%d]\n", getpid());
 		sleep(10);
 	}
 
-	if (i == 3) //父進程
+	if (i == 3)
 	{
 		printf("the father, pid==[%d]\n", getpid());
 		sleep(10);
@@ -64,28 +54,3 @@ int main()
 
 	return 0;
 }
-
-//會產生 2的n次方-1個子進程  2的n次方進程(+一個父進程)
-// int main()
-// {
-// 	int i = 0;
-// 	for (i = 0; i < 3; i++)
-// 	{
-// 		pid_t pid = fork();
-// 		if (pid < 0)
-// 		{
-// 			perror("fork error");
-// 			return -1;
-// 		}
-// 		else if (pid > 0)
-// 		{
-// 			printf("father: pid==[%d]\n", getpid());
-// 			sleep(1);
-// 		}
-// 		else if (pid == 0)
-// 		{
-// 			printf("child: pid==[%d]\n", getpid());
-// 		}
-// 	}
-// 	return 0;
-// }
