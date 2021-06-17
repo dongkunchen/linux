1792f66b35b939a6b7d1c2e33efb64ba6daf926d
dongkunchen <85998522+dongkunchen@users.noreply.github.com>
2021-06-17 11:17:53 +0800

    Update log.c

--- log.c
+++ log.c
@@ -4,10 +4,7 @@ static int  msgopt, wanopt;
 static char msgdatefmt[100], wandatefmt[100], ident_name[100];
 static struct timeval be_stime;
 static FILE *msgfile = NULL, *wanfile = NULL;
-/* ************************************************************************************ */
-/* ************************************************************************************ */
-/* ************************************************************************************ */
-//日志文件初始化,也可以通过msgLogOpen进行初始化
+
 int msgInit(char *pName)
 {
     if (msgLogOpen(pName, LOG_MESSAGE_FILE, LOG_POSTFIX_MESS,LOG_WARNING_FILE, LOG_POSTFIX_WARN) == 0)
@@ -21,8 +18,7 @@ int msgInit(char *pName)
     }
     return 0;
 }
-/* ************************************************************************************ */
-int msgLogOpen(char *ident, char *mpre, char *mdate, char *wpre, char *wdate) /* 打开日志 */
+int msgLogOpen(char *ident, char *mpre, char *mdate, char *wpre, char *wdate) 
 {
     time_t now_time;
     char openfilename[200], timestring[100];
@@ -38,7 +34,7 @@ int msgLogOpen(char *ident, char *mpre, char *mdate, char *wpre, char *wdate) /*
             strcat(openfilename, timestring);
         }
         if ((msgfile = fopen(openfilename, "a+b")) == NULL)
-        { /* 如果没有应该把目录建上 */
+        { 
             printf("openfilename=%s\n", openfilename);
             return -1;
         }
@@ -67,10 +63,10 @@ int msgLogOpen(char *ident, char *mpre, char *mdate, char *wpre, char *wdate) /*
         } else {
             ident_name[0] = '\0';
         }
-        msgopt = LOG_PROCNAME|LOG_PID;          /* 设置默认信息输出信息选项              */
-        wanopt = LOG_PROCNAME|LOG_PID;          /* 设置默认告警输出信息选项              */
-        strcpy(msgdatefmt, "%m-%d %H:%M:%S");   /* 默认信息输出时间格式 MM-DD HH24:MI:SS */
-        strcpy(wandatefmt, "%m-%d %H:%M:%S");   /* 默认告警输出时间格式 MM-DD HH24:MI:SS */
+        msgopt = LOG_PROCNAME|LOG_PID;          
+        wanopt = LOG_PROCNAME|LOG_PID;          
+        strcpy(msgdatefmt, "%m-%d %H:%M:%S");   
+        strcpy(wandatefmt, "%m-%d %H:%M:%S");   
 
         msglog(MSG_INFO,"File is msgfile=[%d],wanfile=[%d].",fileno(msgfile),fileno(wanfile));
         return 0;
@@ -78,20 +74,16 @@ int msgLogOpen(char *ident, char *mpre, char *mdate, char *wpre, char *wdate) /*
         return -1;
     }
 }
-/* ************************************************************************************ */
-/* 自定义日志输出函数系列,可以按普通信息及告警信息分类输出程序日志                      */
 int msglog(int mtype, char *outfmt, ...)
 {
     time_t now_time;
-    va_list ap;//变参的列表
+    va_list ap;
     char logprefix[1024], tmpstring[1024];
 
     time(&now_time);
     if (mtype & MSG_INFO)
-    { /*strftime会将localtime(&now_time)按照msgdatefmt格式,输出到logprefix.*/
         strftime(logprefix, sizeof(logprefix), msgdatefmt, localtime(&now_time));
         strcat(logprefix, " ");
-        /*static int  msgopt,wanopt;*/
         if (msgopt&LOG_PROCNAME)
         {
             strcat(logprefix, ident_name);
@@ -112,7 +104,6 @@ int msglog(int mtype, char *outfmt, ...)
     {
         strftime(logprefix, sizeof(logprefix), wandatefmt, localtime(&now_time));
         strcat(logprefix, " ");
-        /*#define LOG_PROCNAME      0x00000001*/              /* msglog 输出日志时打印程序名        */
         if (wanopt & LOG_PROCNAME)
         {
             strcat(logprefix, ident_name);
@@ -140,8 +131,7 @@ int msglog(int mtype, char *outfmt, ...)
 
     return 0;
 }
-/* ************************************************************************************ */
-int msgLogFormat(int mopt, char *mdfmt, int wopt, char *wdfmt)   /* 设置日志格式及选项  */
+int msgLogFormat(int mopt, char *mdfmt, int wopt, char *wdfmt) 
 {
     if (mopt >= 0)
     {
@@ -161,23 +151,20 @@ int msgLogFormat(int mopt, char *mdfmt, int wopt, char *wdfmt)   /* 设置日志
 
     return 0;
 }
-/* ************************************************************************************ */
-int msgLogClose(void)                                           /* 关闭日志文件         */
+int msgLogClose(void)
 {
     if (msgfile) fclose(msgfile);
     if (wanfile) fclose(wanfile);
 
     return 0;
 }
-/* ************************************************************************************ */
-long begusec_process(void)                      /* 设置开始时间 0=ok                    */
+long begusec_process(void)
 {
     gettimeofday(&be_stime,NULL);
 
     return 0;
 }
-/* ************************************************************************************ */
-long getusec_process(void)                      /* 返回usecond 从 begusec_process历时   */
+long getusec_process(void)                      
 {
     struct timeval ed_stime;
 
@@ -185,7 +172,3 @@ long getusec_process(void)                      /* 返回usecond 从 begusec_pro
 
     return ((ed_stime.tv_sec-be_stime.tv_sec)*1000000+ed_stime.tv_usec-be_stime.tv_usec);
 }
-/* ************************************************************************************ */
-/* ************************************************************************************ */
-/* ************************************************************************************ */
-/* ************************************************************************************ */
