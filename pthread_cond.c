f58bbb4e5f81189adc49dbcd02ce7cfbc99b82f5
dongkunchen <85998522+dongkunchen@users.noreply.github.com>
2021-06-17 11:31:43 +0800

    Update pthread_cond.c

--- pthread_cond.c
+++ pthread_cond.c
@@ -1,4 +1,3 @@
-//線程同步-互斥鎖
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -14,20 +13,16 @@ typedef struct node
 
 NODE *head = NULL;
 
-//定義一把互斥鎖
 pthread_mutex_t mutex;
 
-//定義條件變量
 pthread_cond_t cond;
 
-//生產者線程
 void *producer(void *arg)
 {
 
     NODE *pNode = NULL;
     while (1)
     {
-        //生產一個節點
         pNode = (NODE *)malloc(sizeof(NODE));
         if (pNode == NULL)
         {
@@ -37,35 +32,27 @@ void *producer(void *arg)
         pNode->data = rand() % 1000;
         printf("P:[%d]\n", pNode->data);
 
-        //加鎖
         pthread_mutex_lock(&mutex);
         pNode->next = head;
         head = pNode;
 
-        //解鎖
         pthread_mutex_unlock(&mutex);
 
-        //解鎖之後再通知消費者解除阻塞
         pthread_cond_signal(&cond);
 
         sleep(rand() % 3);
     }
 }
 
-//消費者線程
 void *consumer(void *arg)
 {
     NODE *pNode = NULL;
     while (1)
     {
-        //加鎖
         pthread_mutex_lock(&mutex);
 
         if (head == NULL)
         {
-            //若條件不滿足需要阻塞等待
-            //若條件不滿足則阻塞等帶並解鎖
-            //若條件滿足(被生產者線程調用pthread_cond_signal函數通知)解除阻塞並加鎖
             pthread_cond_wait(&cond, &mutex);
         }
 
@@ -73,14 +60,12 @@ void *consumer(void *arg)
         pNode = head;
         head = head->next;
 
-        //解鎖
         pthread_mutex_unlock(&mutex);
 
         free(pNode);
         pNode = NULL;
         sleep(rand() % 3);
     }
-    // pthread_exit(NULL);
 }
 
 int main()
@@ -89,13 +74,10 @@ int main()
     pthread_t thread1;
     pthread_t thread2;
 
-    //互斥鎖初始化要在主線程內
     pthread_mutex_init(&mutex, NULL);
 
-    //條件變量初始化
     pthread_cond_init(&cond, NULL);
 
-    //創建生產者線程
     ret = pthread_create(&thread1, NULL, producer, NULL);
     if (ret != 0)
     {
@@ -103,7 +85,6 @@ int main()
         return -1;
     }
 
-    //創建消費者線程
     ret = pthread_create(&thread2, NULL, consumer, NULL);
     if (ret != 0)
     {
@@ -114,10 +95,8 @@ int main()
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
 
-    //釋放互斥鎖
     pthread_mutex_destroy(&mutex);
 
-    //釋放條件變量
     pthread_cond_destroy(&cond);
 
     return 0;
