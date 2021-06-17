e2714de6c5058bc3991776afe310142d89608183
dongkunchen <85998522+dongkunchen@users.noreply.github.com>
2021-06-17 11:32:57 +0800

    Update pthread_detach.c

--- pthread_detach.c
+++ pthread_detach.c
@@ -1,4 +1,3 @@
-//設置子線程為分離屬性
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -22,16 +21,13 @@ int main()
     }
     printf("main thread, pid==[%d], id==[%ld]\n", getpid(), pthread_self());
 
-    //設置線程為分離屬性
     pthread_detach(thread);
 
-    //驗證是否分離可調用join測試
     ret = pthread_join(thread, NULL);
     if(ret!=0)
     {
         printf("pthread_join error:[%s]\n",strerror(ret));
     }
-    //目的是為了回收前讓子線程執行起來
     sleep(1);
     return 0;
 }
