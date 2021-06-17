cffb7f5b801cdee14588a7a291968e8397a46a83
dongkunchen <85998522+dongkunchen@users.noreply.github.com>
2021-06-17 11:45:27 +0800

    Update threadpoolsimple.h

--- threadpoolsimple.h
+++ threadpoolsimple.h
@@ -10,32 +10,32 @@
 
 typedef struct _PoolTask
 {
-    int tasknum;//模擬任務編號
-    void *arg;//回調函數參數
-    void (*task_func)(void *arg);//任務的回調函數    
+    int tasknum;
+    void *arg;
+    void (*task_func)(void *arg);
 }PoolTask;
 
 typedef struct _ThreadPool
 {
-    int max_job_num;//最大任務個數
-    int job_num;//實際任務個數
-    PoolTask *tasks;//任務對列數組
-    int job_push;//入隊位置
-    int job_pop;//出隊位置
+    int max_job_num;
+    int job_num;
+    PoolTask *tasks;
+    int job_push;
+    int job_pop;
 
-    int thr_num;//線程池內線程個數
-    pthread_t *threads;//線程池內線程數組
-    int shutdown;//是否關閉線程池
-    pthread_mutex_t pool_lock;//線程池的鎖
-    pthread_cond_t empty_task;//任務隊列為空的條件
-    pthread_cond_t not_empty_task;//任務隊列不為空的條件
+    int thr_num;
+    pthread_t *threads;
+    int shutdown;
+    pthread_mutex_t pool_lock;
+    pthread_cond_t empty_task;
+    pthread_cond_t not_empty_task;
 
 }ThreadPool;
 
-void create_threadpool(int thrnum, int maxtasknum);//創建線程池--thrnum 代表線程個數
-void destroy_threadpool(ThreadPool *pool);//摧毀線程池
-void addtask(ThreadPool *pool);//添加任務到線程池
-void taskRun(void *arg);//任務回調函數
+void create_threadpool(int thrnum, int maxtasknum);
+void destroy_threadpool(ThreadPool *pool);
+void addtask(ThreadPool *pool);
+void taskRun(void *arg);
 
 #endif
 
