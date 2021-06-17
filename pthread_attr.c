b3107b51f2ad48e7e7f40c43b82fa1bafeb1f7e7
dongkunchen <85998522+dongkunchen@users.noreply.github.com>
2021-06-17 11:30:34 +0800

    Update pthread_attr.c

--- pthread_attr.c
+++ pthread_attr.c
@@ -1,4 +1,3 @@
-//創建線程時設置分離屬性
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -14,16 +13,12 @@ void *mythread(void *arg)
 
 int main()
 {
-    //定義pthread_attr_t類型的變量;
     pthread_attr_t attr;
 
-    //初始化attr變量
     pthread_attr_init(&attr);
 
-    //設置attr為分離屬性
     pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
 
-    //創建子線程
     pthread_t thread;
     int ret = pthread_create(&thread, &attr , mythread, NULL);
     if(ret!=0)
@@ -33,10 +28,8 @@ int main()
     }
     printf("main thread, pid==[%d], id==[%ld]\n", getpid(), pthread_self());
 
-    //釋放線程屬性
     pthread_attr_destroy(&attr);
 
-    //驗證如果線程阻塞就是非分離 沒阻塞就是分離 用sleep看有無立刻返回
     ret = pthread_join(thread, NULL);
     if(ret!=0)
     {
