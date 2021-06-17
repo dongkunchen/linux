98cf83c4942e1000dbc88a51c93ddbe179994b7d
dongkunchen <85998522+dongkunchen@users.noreply.github.com>
2021-06-17 11:19:29 +0800

    Update log.h

--- log.h
+++ log.h
@@ -1,6 +1,5 @@
 #ifndef LOG_H
 #define LOG_H
-/* ************************************************************************************ */
 
 #include <dirent.h>
 #include <errno.h>
@@ -21,36 +20,31 @@
 #include <sys/time.h>
 #include <unistd.h>
 
-#define LOG_PROCNAME      0x00000001              /* msglog 输出日志时打印程序名        */
-#define LOG_PID           0x00000010              /* msglog 输出日志时打印进程 PID      */
-#define LOG_PERROR        0x00000100              /* msglog 是否把告警内容输出到stderr  */
-#define NLO_PROCNAME      0x11111110              /* msglog 不输出程序名                */
-#define NLO_PID           0x11111101              /* msglog 不输出进程 PID              */
-#define NLO_PERROR        0x11111011              /* msglog 不输出告警到stderr          */
+#define LOG_PROCNAME      0x00000001              
+#define LOG_PID           0x00000010              
+#define LOG_PERROR        0x00000100              
+#define NLO_PROCNAME      0x11111110              
+#define NLO_PID           0x11111101              
+#define NLO_PERROR        0x11111011             
 
-#define MSG_INFO          0x00000001              /* msglog 输出到告警日志文件中        */
-#define MSG_WARN          0x00000010              /* msglog 输出到普通日志文件中        */
-#define MSG_BOTH          MSG_INFO|MSG_WARN       /* msglog 输出到普通和告警日志文件中  */
+#define MSG_INFO          0x00000001              
+#define MSG_WARN          0x00000010              
+#define MSG_BOTH          MSG_INFO|MSG_WARN       
 
-#define LOG_MESSAGE_FILE  "/home/itheima/log/tcpsvr"           /* 系统程序运行日志信息文件           */
-#define LOG_MESSAGE_DFMT  "%m-%d %H:%M:%S"        /* 日志信息时间格式字串               */
-#define LOG_POSTFIX_MESS  "%y%m"                  /* 程序运行日志信息文件后缀           */
-#define LOG_WARNING_FILE  "/home/itheima/log/log.sys_warn"   /* 系统程序运行告警日志文件           */
-#define LOG_WARNING_DFMT  "%m-%d %H:%M:%S"        /* 告警信息时间格式字串               */
-#define LOG_POSTFIX_WARN  ""                      /* 程序运行告警日志文件后缀           */
+#define LOG_MESSAGE_FILE  "/home/itheima/log/tcpsvr"           
+#define LOG_MESSAGE_DFMT  "%m-%d %H:%M:%S"        
+#define LOG_POSTFIX_MESS  "%y%m"                  
+#define LOG_WARNING_FILE  "/home/itheima/log/log.sys_warn"   
+#define LOG_WARNING_DFMT  "%m-%d %H:%M:%S"       
+#define LOG_POSTFIX_WARN  ""    
 
-/* ************************************************************************************ */
-int msglog(int mtype, char *outfmt, ...);//写日志函数
-int msgLogFormat(int mopt, char *mdfmt, int wopt, char *wdfmt);//对日志格式化 
-int msgLogOpen(char *ident, char *mpre, char *mdate, char *wpre, char *wdate);//打开日志文件
-int msgLogClose(void);//关闭日志文件
+int msglog(int mtype, char *outfmt, ...);
+int msgLogFormat(int mopt, char *mdfmt, int wopt, char *wdfmt); 
+int msgLogOpen(char *ident, char *mpre, char *mdate, char *wpre, char *wdate);
+int msgLogClose(void);
 
-long begusec_process(void);                      /* 设置开始时间 0=ok                   */
-long getusec_process(void);                      /* 返回usecond 从 begusec_process历时  */
+long begusec_process(void);                      
+long getusec_process(void);                      
 
 int msgInit(char *pName);
 #endif
-/* ************************************************************************************ */
-/* ************************************************************************************ */
-/* ************************************************************************************ */
-/* ************************************************************************************ */
