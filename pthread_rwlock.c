6917134bbdf6817e7f63e641a7cff0ea021a21d2
dongkunchen <85998522+dongkunchen@users.noreply.github.com>
2021-06-17 11:50:11 +0800

    Update pthread_rwlock.c

--- pthread_rwlock.c
+++ pthread_rwlock.c
@@ -1,4 +1,3 @@
-//讀寫鎖測試程序
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,24 +7,20 @@
 
 int number = 0;
 
-//定義一把讀寫鎖
 pthread_rwlock_t rwlock;
 
-//寫線程回調函數
 void *thread_write(void *arg)
 {
     int i = *(int *)arg;
     int cur;
     while(1)
     {
-        //加寫鎖
         pthread_rwlock_wrlock(&rwlock);
         cur = number;
         cur++;
         number = cur;
         printf("[%d]-W:[%d]\n", i, cur);
 
-        //解鎖
         pthread_rwlock_unlock(&rwlock);
         sleep(rand()%3);
     }
@@ -37,13 +32,11 @@ void *thread_read(void *arg)
     int cur;
     while(1)
     {
-        //加讀鎖
         pthread_rwlock_rdlock(&rwlock);
 
         cur = number;
         printf("[%d]-R:[%d]\n", i, cur);
         
-        //解鎖加在等待上面
         pthread_rwlock_unlock(&rwlock);
         sleep(rand()%3);
 
@@ -52,22 +45,18 @@ void *thread_read(void *arg)
 
 int main()
 {
-    //創建子線程
     int n = 8;
     int i = 0;
     int arr[8];
     pthread_t thread[8];
 
-    //初始化讀寫鎖
     pthread_rwlock_init(&rwlock, NULL);
 
-    //創建三個寫子線程
     for (i = 0; i < 3; i++)
     {
         arr[i] = i;
         pthread_create(&thread[i], NULL, thread_write, &arr[i]);
     }
-    //創建五個讀子線程
     for (i = 3; i < n; i++)
     {
         arr[i] = i;
