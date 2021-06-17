4844ec98e311ee733b81a39a841d6470fd89a38b
dongkunchen <85998522+dongkunchen@users.noreply.github.com>
2021-06-17 11:58:10 +0800

    Update pub.h

--- pub.h
+++ pub.h
@@ -11,7 +11,7 @@
 #include <ctype.h>
 char *get_mime_type(char *name);
 int get_line(int sock, char *buf, int size);
-int hexit(char c);//16进制转10进制
-void strencode(char* to, size_t tosize, const char* from);//编码
-void strdecode(char *to, char *from);//解码
+int hexit(char c);
+void strencode(char* to, size_t tosize, const char* from);
+void strdecode(char *to, char *from);
 #endif
