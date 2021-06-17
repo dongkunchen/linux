bb46d8c93416c2c5b2c3e0c41e36e04ba3b41bb4
dongkunchen <85998522+dongkunchen@users.noreply.github.com>
2021-06-17 11:11:04 +0800

    Update fifo_write.c

--- fifo_write.c
+++ fifo_write.c
@@ -1,4 +1,3 @@
-//fifo完成兩個進程間通信測試
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,10 +8,7 @@
 
 int main()
 {
-    //創建fifo文件
-    // int ret = mkfifo("./myfifo", 0777);
     int ret = access("./myfifo", F_OK);
-    //先利用access函數判斷是否存在0存在非0不存在創建
     if (ret != 0)
     {
         ret = mkfifo("./myfifo", 0777);
@@ -23,7 +19,6 @@ int main()
         }
     }
 
-    //打開文件
     int fd = open("./myfifo", O_RDWR);
     if (fd < 0)
     {
@@ -31,7 +26,6 @@ int main()
         return -1;
     }
 
-    //寫fifo文件
     int i = 0;
     char buf[64];
     while (1)
@@ -42,11 +36,8 @@ int main()
         sleep(1);
         i++;
     }
-    //關閉文件
     close(fd);
 
-    //等待輸入------暫停類似systme("pause"); linux無法用system
-    //getchar();
 
     return 0;
 }
